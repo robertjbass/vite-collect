0519a6daf3458d1a8d2f6e3bad37cc1fbfdce58c
Bob Bass <bob@bbass.co>
2020-05-03 06:39:59 -0400

    created first build

--- components/Main.vue
+++ components/Main.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    <h1>This is my Vite Main Component</h1>
-    <h3>Vite + Vue 3!</h3>
-    <p>
-      <span>Count is: {{ count }}</span
-      ><br />
-      <button @click="count++">increment</button><br />
-      <button @click="count--">decrement</button>
-    </p>
-  </div>
+  <h1>This is my Vite Main Component</h1>
+  <h3>Vite + Vue 3!</h3>
+  <p>
+    <span>Count is: {{ count }}</span>
+    <br />
+    <button @click="count++">increment</button><br />
+    <button @click="count--">decrement</button>
+  </p>
 </template>
 
 <script>
